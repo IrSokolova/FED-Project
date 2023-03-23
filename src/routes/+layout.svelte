c6dfda80e6999c11751cc62f52c69d2d5518299d
IrSokolova <71442225+IrSokolova@users.noreply.github.com>
2023-03-23 10:14:47 +0300

    HW5

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import {base} from "$app/paths";
+</script>
+
+<header class="lay-h">
+    <a href='{base}/'> Home </a>
+</header>
+<slot />
+<footer class="foot-h">
+    <a href='https://www.youtube.com/watch?v=j5a0jTc9S10&list=PLNTK9welGzDynukDlMqLGqY3rlaMPOJOl&index=10'> Cute Little Puppy Doing Cute things </a>
+</footer>
+<style>
+    .lay-h {
+        background-color: antiquewhite;
+        text-align: center;
+        color: blueviolet;
+        margin-left: 20px;
+    }
+
+    .foot-h {
+        background-color: antiquewhite;
+        text-align: center;
+        color: blueviolet;
+        margin-left: 20px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import {base} from "$app/paths";
+</script>
+
+<body>
+<h1>Sokolova's Irina website</h1>
+
+<div class="navigation">
+    <p><a href='{base}/bio'> About Me </a></p>
+    <p><a href='{base}/contacts'> Social Info </a></p>
+    <p><a href='{base}/comic'> Click if you want to get a comic </a></p>
+</div>
+</body>
+
+
+<style>
+    h1 {
+        color: blueviolet;
+        margin-left: 20px;
+        text-align: center;
+    }
+
+    p {
+        font-size: 20px;
+        color: dodgerblue;
+        margin-left: 300px;
+        margin-right: 300px;
+        margin-bottom: 20px;
+        text-align: center;
+        background-color: #acff8e;
+    }
+
+    body {
+        background-color: #c8fdff;
+    }
+</style>
